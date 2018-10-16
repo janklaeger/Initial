2be9ce5a8020db8dbe99a4c2f56a328bcfbdff73
Jan Kläger <jan@klaegerag.ch>
2018-10-17 00:02:38 +0200

    raw website

---
+++ webpage/index.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Doodleville</title>
+    <link rel="stylesheet" href="stylesheet/stylesheet_main.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    <script src="javascript/scroll.js"></script>
+</head>
+<body>
+<!-- Navigation Bar -->
+    <nav>
+        <a href="#home">Home</a>
+        <a href="#aboutus">About Us</a>
+        <a href="#blog">Blog</a>
+        <a href="#contact">Contact</a>
+    </nav>
+<div id="contentarea">
+<!-- Home           -->
+    <section id="home">
+        <h2>Home</h2>
+        <hr>
+    </section>
+<!-- About Us       -->
+    <section id="aboutus">
+        <h2>About Us</h2>
+        <hr>
+    </section>
+<!-- Blog           -->
+    <section id="blog">
+        <h2>Blog</h2>
+        <hr>
+    </section>
+<!-- Contact        -->
+    <section id ="contact">
+        <h2>Contact</h2>
+        <hr>
+
+        <form method="post">
+            <input type="text">
+            <input type="email">
+            <textarea cols="30" rows="10"></textarea>
+        </form>
+    </section>
+</div>
+
+<!-- End Body       -->
+</body>
+</html>
